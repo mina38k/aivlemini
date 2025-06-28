5fbc760f6db7d431fcac10597a7341a2d4296835
mina38k <105428209+mina38k@users.noreply.github.com>
2025-06-28 18:25:48 +0000

    쿼리 수정

--- libraryservice/src/main/java/aivlemini/domain/BookRepository.java
+++ libraryservice/src/main/java/aivlemini/domain/BookRepository.java
@@ -14,18 +14,30 @@ public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
     @Query(
         value = "select book " +
         "from Book book " +
-        "where(:id is null or book.id = :id) and (:bookName is null or book.bookName like %:bookName%) and (:category is null or book.category like %:category%) and (:isBestSeller is null or book.isBestSeller = :isBestSeller) and (:authorName is null or book.authorName like %:authorName%) and (:image is null or book.image like %:image%) and (:subscriptionCount is null or book.subscriptionCount = :subscriptionCount) and (:bookContent is null or book.bookContent like %:bookContent%) and (:authorId is null or book.authorId like %:authorId%) and (:pdfPath is null or book.pdfPath like %:pdfPath%)"
+        "where(:id is null or book.id = :id) and (:bookName is null or book.bookName like concat('%', :bookName, '%')) and (:category is null or book.category like concat('%', :category, '%')) and (:isBestSeller is null or book.isBestSeller = :isBestSeller) and (:authorName is null or book.authorName like concat('%', :authorName, '%')) and (:image is null or book.image like concat('%', :image, '%')) and (:subscriptionCount is null or book.subscriptionCount = :subscriptionCount) and (:bookContent is null or book.bookContent like concat('%', :bookContent, '%')) and (:authorId is null or book.authorId like concat('%', :authorId, '%')) and (:pdfPath is null or book.pdfPath like concat('%', :pdfPath, '%'))"
     )
-    Book getBooks(
-        Long id,
-        String bookName,
-        String category,
-        Boolean isBestSeller,
-        String authorName,
-        String image,
-        Integer subscriptionCount,
-        String bookContent,
-        String authorId,
-        String pdfPath
-    );
+    // Book getBooks(
+    //     Long id,
+    //     String bookName,
+    //     String category,
+    //     Boolean isBestSeller,
+    //     String authorName,
+    //     String image,
+    //     Integer subscriptionCount,
+    //     String bookContent,
+    //     String authorId,
+    //     String pdfPath
+    // ); 
+    List<Book> findBooks(
+    Long id,
+    String bookName,
+    String category,
+    Boolean isBestSeller,
+    String authorName,
+    String image,
+    Integer subscriptionCount,
+    String bookContent,
+    String authorId,
+    String pdfPath
+);
 }
